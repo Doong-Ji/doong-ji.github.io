8869823accbe2a0320bce0b9ecbc9aeb8f22613c
snr <nr.shin000@gmail.com>
2021-08-02 23:14:13 +0900

    chore : member_project slide add next,prev btn content width 1080 -> 1180

--- client/public/pu/about.js
+++ client/public/pu/about.js
@@ -1,15 +1,16 @@
 var swiper = new Swiper('.mySwiper', {
   slidesPerView: 2,
+  spaceBetween: 30,
   breakpoints: {
     // when window width is <= 499px
     1200: {
       slidesPerView: 2,
-      spaceBetweenSlides: 30,
+      spaceBetween: 30,
     },
     // when window width is <= 999px
     768: {
       slidesPerView: 1,
-      spaceBetweenSlides: 40,
+      spaceBetween: 40,
     },
   },
 });
@@ -19,17 +20,18 @@ var reviewSwiper = new Swiper('.reviewSwiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  slidesPerView: 3,
+  slidesPerView: 3.2,
+  spaceBetween: 22,
   breakpoints: {
-    // when window width is <= 499px
+    // when window width is <= 1200px
     1200: {
       slidesPerView: 2,
-      spaceBetweenSlides: 30,
+      spaceBetween: 22,
     },
-    // when window width is <= 999px
+    // when window width is <= 768px
     768: {
-      slidesPerView: 1,
-      spaceBetweenSlides: 100,
+      slidesPerView: 1.1,
+      spaceBetween: 20,
     },
   },
 });
